90ec131a470877248b1022b41f95ac6222776b9d
Luís Serrano <luisgserrano.d@gmail.com>
2020-03-11 01:46:50 +0000

    Fix config to deploy package to npm

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,22 +1,32 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import sourceMaps from 'rollup-plugin-sourcemaps';
-import camelCase from 'lodash.camelcase';
 import typescript from 'rollup-plugin-typescript2';
 import json from 'rollup-plugin-json';
 
 const pkg = require('./package.json');
 
-const libraryName = 'react-multi-modal';
-
 export default {
-  input: `src/${libraryName}.tsx`,
+  input: 'src/index.tsx',
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true,
+    },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+    'react',
+    'react-dom',
+  ],
   watch: {
     include: 'src/**',
   },
@@ -26,11 +36,19 @@ export default {
     // Compile TypeScript files
     typescript({ typescript: require('typescript'), useTsconfigDeclarationDir: true }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
+    commonjs({
+      include: ['node_modules/**'],
+      namedExports: {
+        'node_modules/react/react.js': ['Children', 'Component', 'PropTypes', 'createElement'],
+        'node_modules/react-dom/index.js': ['render'],
+      },
+    }),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
+    resolve({
+      browser: true,
+    }),
 
     // Resolve source maps to the original source
     sourceMaps(),
